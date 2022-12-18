cf85f87059e7a4827bcba45af5634f6d37ef5b8e
alvaro <alvarodr1983@gmail.com>
2022-12-18 01:32:15 +0100

    add aspect for logging

--- src/main/java/com/quant/ecommerce/aspect/AspectLogging.java
+++ src/main/java/com/quant/ecommerce/aspect/AspectLogging.java
@@ -3,7 +3,6 @@ package com.quant.ecommerce.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
